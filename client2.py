2de3404c716cb28c3794a301f2c5f898a14e4770
GAURAV KUMAR <gauravkumar22927@gmail.com>
2019-08-13 06:48:43 +0530

    first commit

---
+++ client2.py
@@ -0,0 +1,36 @@
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNStatusCategory
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+import time
+import os
+
+pnconfig = PNConfiguration()
+
+pnconfig.publish_key = 'pub-c-3abf21e5-b61e-48bb-9101-be84c7fbe438'
+pnconfig.subscribe_key = 'sub-c-87f59912-b4e1-11e9-b6f7-eea0353b68c5'
+pnconfig.ssl = True
+
+pubnub = PubNub(pnconfig)
+
+def my_publish_callback(envelope, status):
+    # Check whether request successfully completed or not
+    if not status.is_error():
+        pass
+
+class MySubscribeCallback(SubscribeCallback):
+    def presence(self, pubnub, presence):
+        pass
+    def status(self, pubnub, status):
+        pass
+    def message(self, pubnub, message):
+        print("from device 1: " + message.message)
+
+pubnub.add_listener(MySubscribeCallback())
+pubnub.subscribe().channels("chan-1").execute()
+
+## publish a message
+while True:
+    msg = input("Input a message to publish: ")
+    if msg == 'exit': os._exit(1)
+    pubnub.publish().channel("chan-1").message(str(msg)).pn_async(my_publish_callback)
